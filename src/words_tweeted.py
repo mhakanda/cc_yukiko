2fb76a28449a9972f3494fbe4808b849717ecc37
baramuyu <baramuyu@gmail.com>
2015-07-06 23:05:16 -0700

    task1

--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -1,1 +1,41 @@
-# example of program that calculates the total number of times each word has been tweeted.
+from sys import argv
+import collections
+
+script, input1, input2 = argv
+tweets = []
+words = []
+index = {}
+maxlen = 0
+
+fIn = open(input1, 'r')
+
+#read input file
+for line in fIn:
+	tweets.append(line)
+
+fIn.close()
+
+def add_to_index(index, keyword):
+	global maxlen
+	if keyword in index: #keyword is existing in index
+		index[keyword] += 1 #increment word count 
+	else: #create index with new keyword
+		index[keyword] = 1
+		if len(keyword) > maxlen: #get max length among all words
+			maxlen = len(keyword) 
+
+for tw in tweets:
+	words = tw.split() #break down to each word
+	for word in words:
+		add_to_index(index, word) 
+		
+#sorting by alphabetical order
+sortedIndex = collections.OrderedDict(sorted(index.items()))
+
+#create output file
+fOut = open(input2,'w')
+for word, count in sortedIndex.iteritems():
+	sp = maxlen - len(word) + 6 #space for indent
+	fOut.write(word + (' ' * sp) + str(count) + '\n')
+
+fOut.close()
